967ca11e1c5faa0bba284a7d63c1edca587aa2f7
titas251 <krtitas1@gmail.com>
2022-07-18 14:22:18 +0300

    Moved db connection options

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Movie } from './Modules/Movie/Movie.entity';
+import { ConnectionOptions } from './TypeORM/ConnectionOptions';
 import { MovieModule } from './Modules/Movie/Movie.module';
-
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.MYSQL_HOST,
-      port: parseInt(process.env.MYSQL_PORT),
-      username: process.env.MYSQL_USERNAME,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
-      entities: [Movie],
-      synchronize: true,
-    }),
-    MovieModule,
-  ],
+  imports: [TypeOrmModule.forRoot(ConnectionOptions), MovieModule],
 })
 export class AppModule {}
